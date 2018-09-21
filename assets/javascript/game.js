d9537278db6bcc4e9df42046e4a548d8cfe89efa
Jonathan Bald <baldyj117@yahoo.com>
2018-09-21 17:07:50 -0500

    addedalert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
- var letterOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
- 
+var letterOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 
 var wins = 0;
 var losses = 0;
@@ -10,34 +10,47 @@ var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.lengt
 
 
 
-document.onkeyup = function(event) {
-    
-    
+document.onkeyup = function (event) {
+
+
     var userGuess = (event.key).toLowerCase();
-   
+    
+
+    if (lettersGuessed.includes(userGuess)) {
+        guessesLeft ++;
+        alert("You already guessed that! Try Again!");
+    }
+
+
 
     if (letterOptions.indexOf(userGuess) > -1) {
         lettersGuessed.push(userGuess);
     }
 
+
+
     if (userGuess === computerGuess) {
         wins++;
         guessesLeft = 10;
         lettersGuessed = [];
-        computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];       
-        }  
-    if (userGuess !== computerGuess){
-        guessesLeft--;   
+        computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
+    }
+    if (userGuess !== computerGuess) {
+        guessesLeft--;
     }
 
-    if (guessesLeft === 0) {   
+    if (guessesLeft === 0) {
         losses++;
         guessesLeft = 9;
         lettersGuessed = [];
-        computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];        
+        computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
     }
 
-    var html = 
+
+
+
+
+    var html =
         "<h1>The Psychic Game</h1>" +
         "<p>Guess what letter I'm thinking of</p>" +
         "<p>Wins: " + wins + "</p>" +
@@ -47,7 +60,7 @@ document.onkeyup = function(event) {
 
     document.getElementById("game").innerHTML = html;
     //innerHTML changes the text in the game when started.
-    
+
     console.log("Your Guess: " + userGuess);
     console.log("Computer Guess: " + computerGuess);
 };
